ee2f3126e1b891ee2bc591f57a49a1df16b2be02
Piotr Kowalski <piecioshka@gmail.com>
2017-05-31 22:39:25 +0200

    Connect AppComponent with DataService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { DataService } from './services/data.service';
+import { RestService } from './services/rest.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [
+    DataService,
+    RestService
+  ]
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor(private dataService: DataService) {
+    this.dataService.fetch();
+  }
 }

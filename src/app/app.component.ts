3582b2f3e67cdcdec13e8dc8387294f75ea897a8
Piotr Kowalski <piecioshka@gmail.com>
2017-06-01 00:23:34 +0200

    Display my GitHub profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
+import { Response } from '@angular/http';
 import { DataService } from './services/data.service';
 import { RestService } from './services/rest.service';
+
+interface GitHubProfileResponse extends Response {
+  avatar_url: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -12,11 +18,14 @@ import { RestService } from './services/rest.service';
   ]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = '';
+  avatar_url = '';
 
   constructor(private dataService: DataService) {
     this.dataService.fetch('https://api.github.com/users/piecioshka')
-      .subscribe((response) => {
+      .subscribe((response: GitHubProfileResponse): void => {
+        this.title = response.name;
+        this.avatar_url = response.avatar_url;
         console.log(response);
       });
   }
